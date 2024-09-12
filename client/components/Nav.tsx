03821492cfad720404524bfe65c0ba42040bfb18
Georgia Harris <ggjune26@gmail.com>
2024-09-12 22:37:54 +1200

    Setting up Authentication

---
+++ client/components/Nav.tsx
@@ -0,0 +1,36 @@
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
+
+function Nav() {
+  const { loginWithRedirect, logout, user } = useAuth0()
+
+  const handleSignOut = () => {
+    logout()
+  }
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
+  return (
+    <>
+      <IfAuthenticated>
+        <button onClick={handleSignOut}>Sign out</button>
+        {user && (
+          <div>
+            <img src={user?.picture} alt="profile pic" />
+            <p>
+              Signed in as: {user?.nickname} / {user?.given_name}
+            </p>
+            <p>Email: {user?.email}</p>
+          </div>
+        )}
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <button onClick={handleSignIn}>Sign in</button>
+      </IfNotAuthenticated>
+    </>
+  )
+}
+
+export default Nav
